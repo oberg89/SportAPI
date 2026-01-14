4e14d81c6d732582809ef8baf29b3c26a99c407c
oberg89 <127205354+oberg89@users.noreply.github.com>
2026-01-14 20:12:26 +0100

    Initial commit

---
+++ src/main/java/se/lagomdevs/sportapi/SportApiApplication.java
@@ -0,0 +1,13 @@
+package se.lagomdevs.sportapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lagomdevs/sportapi/SportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lagomdevs.sportapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
